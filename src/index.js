63c7d0b8301613ac211da1c92df6aea3fcd5e916
Salar Hafezi <salar.hfz@gmail.com>
2021-12-05 11:51:29 +0330

    Adopt me - React create element

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const Pet = (props) => React.createElement(
+  'div',
+  {},
+  [
+    React.createElement('h2', {}, props.name),
+    React.createElement('h2', {}, props.animal),
+    React.createElement('h2', {}, 'Mooly'),
+  ]
+)
+
+const App = () => React.createElement(
+  'div',
+  {},
+  [
+    React.createElement('h1', { id: 'test' }, 'Adopt Me'),
+    React.createElement(Pet, { name: 'Lune', animal: 'dog' }),
+  ]
+);
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
